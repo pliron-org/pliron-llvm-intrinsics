be0a68df68435967a3b7ebff7083749f03b12d3f
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2026-08-10 18:37:06 -0400

    chore: cleanup ty

--- src/mangling.rs
+++ src/mangling.rs
@@ -1,21 +1,17 @@
-use pliron::printable::Printable;
-use thiserror::Error;
-
 use pliron::builtin::types::{FP16Type, FP32Type, FP64Type, IntegerType};
 use pliron::context::Context;
 use pliron::derive::{type_interface, type_interface_impl};
 use pliron::location::Location;
+use pliron::printable::Printable;
 use pliron::result::Result;
-use pliron::r#type::{TypeHandle, type_cast};
+use pliron::r#type::{Type, TypeHandle, type_cast};
 use pliron::verify_err;
 use pliron_llvm::types::VectorType;
+use thiserror::Error;
 
 #[type_interface]
 pub trait LlvmTypeToMangledOverload {
-    fn verify(
-        _op: &dyn pliron::r#type::Type,
-        _ctx: &pliron::context::Context,
-    ) -> pliron::result::Result<()>
+    fn verify(_op: &dyn Type, _ctx: &Context) -> Result<()>
     where
         Self: Sized,
     {
@@ -28,15 +24,7 @@ macro_rules! impl_llvm_type_to_mangled_overload {
     ($src:ty, $self:ident => $body:expr) => {
         #[type_interface_impl]
         impl LlvmTypeToMangledOverload for $src {
-            fn to_mangled_string(&$self, ctx: &Context, loc: Location) -> Result<String> {
-                Ok($body)
-            }
-        }
-    };
-    ($src:ty, $self:ident, $ctx:ident, $loc:ident => $body:expr) => {
-        #[type_interface_impl]
-        impl LlvmTypeToMangledOverload for $src {
-            fn to_mangled_string(&$self, $ctx: &Context, $loc: Location) -> Result<String> {
+            fn to_mangled_string(&$self, _ctx: &Context, _loc: Location) -> Result<String> {
                 Ok($body)
             }
         }
@@ -47,15 +35,15 @@ impl_llvm_type_to_mangled_overload!(IntegerType, self => format!("i{}", self.wid
 impl_llvm_type_to_mangled_overload!(FP16Type, self => "f16".to_string());
 impl_llvm_type_to_mangled_overload!(FP32Type, self => "f32".to_string());
 impl_llvm_type_to_mangled_overload!(FP64Type, self => "f64".to_string());
-impl_llvm_type_to_mangled_overload!(VectorType, self, ctx, loc => {
-    let prefix = if self.is_scalable() {
-        "nx"
-    } else {
-        ""
-    };
-    let (n, elem) = (self.num_elements(), self.elem_type());
-    format!("{prefix}v{n}{}", llvm_mangled_ty(ctx, elem, Location::Unknown)?)
-});
+
+#[type_interface_impl]
+impl LlvmTypeToMangledOverload for VectorType {
+    fn to_mangled_string(&self, ctx: &Context, loc: Location) -> Result<String> {
+        let prefix = if self.is_scalable() { "nx" } else { "" };
+        let (n, elem) = (self.num_elements(), self.elem_type());
+        Ok(format!("{prefix}v{n}{}", llvm_mangled_ty(ctx, elem, loc)?))
+    }
+}
 
 #[derive(Error, Debug)]
 #[error("Unsupported mangled type: {ty}")]
@@ -68,11 +56,8 @@ pub fn llvm_mangled_ty(ctx: &Context, ty: TypeHandle, loc: Location) -> Result<S
     if let Some(ty) = type_cast::<dyn LlvmTypeToMangledOverload>(&*ty.deref(ctx)) {
         ty.to_mangled_string(ctx, loc)
     } else {
-        verify_err!(
-            loc,
-            MangleTypeError {
-                ty: ty.disp(ctx).to_string()
-            }
-        )
+        let ty = ty.disp(ctx).to_string();
+        let error = MangleTypeError { ty };
+        verify_err!(loc, error)
     }
 }
